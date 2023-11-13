500ca96396030067cc40a73416bf26a887fac203
tangqiang <3256224359@qq.com>
2023-11-13 12:37:33 +0800

    v-for简化

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="a" label="aName" width="180" />
-    <el-table-column prop="b" label="bName" width="180" />
-    <el-table-column prop="c" label="cName" width="180" />
-    <el-table-column prop="d" label="dName" width="180" />
-    <el-table-column prop="e" label="eName" width="180" />
-    <el-table-column prop="f" label="fName" width="180" />
-    <el-table-column prop="g" label="gName" width="180" />
-    <el-table-column prop="h" label="hName" width="180" />
-    <el-table-column prop="i" label="iName" width="180" />
+    <el-table-column
+      v-for="column in tableHeaders"
+      :key="column"
+      :prop="column"
+      :label="column + 'Name'"
+      width="180"
+    ></el-table-column>
   </el-table>
 </template>
 
@@ -24,4 +22,5 @@ const tableData = new Array(9).fill({
   h: "2016-05-03",
   i: "Tom",
 });
+const tableHeaders = Object.keys(tableData[0]);
 </script>
